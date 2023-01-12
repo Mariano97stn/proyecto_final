552481c21a7f3a44f3dee0e80d9b4bf306ef3379
Santana Mariano <marianosantana97@gmail.com>
2023-01-12 04:50:49 -0300

    Finalizado de JS y fin del proyecto final

--- JS/main.js
+++ JS/main.js
@@ -1,6 +1,51 @@
 
 $(document).ready(function() {
-  $("In").click(function() {
-      $("p").hide();
+  $("ul.menu_lista li:first").addClass("active");
+  $(".section_all ul").hide();
+  $(".section_all ul:first").show();
+  $("ul.menu_lista li").click(function(){
+    $("ul.menu_lista li").removeClass("active");
+    $(this).addClass("active");
+    $(".section_all ul").hide(0);
+    var menuSeleccionado = $(this).attr("href");
+    $(menuSeleccionado).show(1000);
+    return false;
+  })
+  
+  /*$(".Inicio").click(function(){
+    $(".section_ini").show(500);
+    $(".section_formac").hide(0);
+    $(".section_formad").hide(0);
+    $(".section_exp").hide(0);
+    $(".section_cont").hide(0);
   });
+  $(".FormacionAcademica").click(function(){
+    $(".section_ini").hide(1);
+    $(".section_formac").show(500);
+    $(".section_formad").hide(1);
+    $(".section_exp").hide(1);
+    $(".section_cont").hide(1);
+  });
+  $(".FormacionAdicional").click(function(){
+    $(".section_ini").hide(1);
+    $(".section_formac").hide(1);
+    $(".section_formad").show(500);
+    $(".section_exp").hide(1);
+    $(".section_cont").hide(1);
+  });
+  $(".ExperienciaLaboral").click(function(){
+    $(".section_ini").hide(1);
+    $(".section_formac").hide(1);
+    $(".section_formad").hide(1);
+    $(".section_exp").show(500);
+    $(".section_cont").hide(1);
+  });
+  $(".Contacto").click(function(){
+    $(".section_ini").hide(1);
+    $(".section_formac").hide(1);
+    $(".section_formad").hide(1);
+    $(".section_exp").hide(1);
+    $(".section_cont").show(500);
+  });*/
 });
+
